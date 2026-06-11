268874da89739945932cd2cd2e453bab546bb63e
SolVerNA <gizartv@gmail.com>
2026-06-12 01:39:11 +0300

    fix(warp): согласование ссылок CPack/упаковки с новыми именами AWARP

--- cmake/CPack.cmake
+++ cmake/CPack.cmake
@@ -1,20 +1,20 @@
-set(CPACK_PACKAGE_VENDOR            AmneziaVPN)
+set(CPACK_PACKAGE_VENDOR            AWARP)
 set(CPACK_PACKAGE_VERSION           ${AMNEZIAVPN_VERSION})
 if(WIN32)
-    set(CPACK_PACKAGE_FILE_NAME "AmneziaVPN_${AMNEZIAVPN_VERSION}_windows_x64")
+    set(CPACK_PACKAGE_FILE_NAME "AWARP_${AMNEZIAVPN_VERSION}_windows_x64")
 elseif(APPLE AND NOT IOS AND NOT MACOS_NE)
-    set(CPACK_PACKAGE_FILE_NAME "AmneziaVPN_${AMNEZIAVPN_VERSION}_macos_x64")
+    set(CPACK_PACKAGE_FILE_NAME "AWARP_${AMNEZIAVPN_VERSION}_macos_x64")
 elseif(LINUX AND NOT ANDROID)
-    set(CPACK_PACKAGE_FILE_NAME "AmneziaVPN_${AMNEZIAVPN_VERSION}_linux_x64")
+    set(CPACK_PACKAGE_FILE_NAME "AWARP_${AMNEZIAVPN_VERSION}_linux_x64")
 endif()
-set(CPACK_PACKAGE_INSTALL_DIRECTORY AmneziaVPN)
-set(CPACK_PACKAGE_EXECUTABLES       AmneziaVPN AmneziaVPN)
+set(CPACK_PACKAGE_INSTALL_DIRECTORY AWARP)
+set(CPACK_PACKAGE_EXECUTABLES       AWARP AWARP)
 set(CPACK_PRE_BUILD_SCRIPTS         ${CMAKE_CURRENT_LIST_DIR}/sign_binaries.cmake)
 set(CPACK_POST_BUILD_SCRIPTS        ${CMAKE_CURRENT_LIST_DIR}/sign_packages.cmake)
 set(CPACK_PROJECT_CONFIG_FILE       ${CMAKE_CURRENT_LIST_DIR}/CPackOptions.cmake)
 set(CPACK_RESOURCE_FILE_LICENSE     ${CMAKE_SOURCE_DIR}/deploy/data/LICENSE.txt)
 
-list(PREPEND CPACK_COMPONENTS_ALL AmneziaVPN)
+list(PREPEND CPACK_COMPONENTS_ALL AWARP)
 
 if(APPLE)
     set(CPACK_GENERATOR productbuild)
@@ -23,8 +23,8 @@ else()
 endif()
 
 # === CPack IFW generator settings ===
-set(CPACK_IFW_PACKAGE_NAME                          AmneziaVPN)
-set(CPACK_IFW_PACKAGE_TITLE                         AmneziaVPN)
+set(CPACK_IFW_PACKAGE_NAME                          AWARP)
+set(CPACK_IFW_PACKAGE_TITLE                         AWARP)
 set(CPACK_IFW_PACKAGE_WIZARD_DEFAULT_WIDTH          600)
 set(CPACK_IFW_PACKAGE_WIZARD_DEFAULT_HEIGHT         380)
 set(CPACK_IFW_PACKAGE_WIZARD_STYLE                  Modern)
@@ -46,22 +46,22 @@ list(APPEND CPACK_WIX_PATCH_FILE    "${_AMNEZIA_WIX_PATCH_SERVICE_CMAKE}" "${_AM
 list(APPEND CPACK_WIX_EXTENSIONS    "WixToolset.Util.wixext")
 
 # === CPack productbuild generator settings ===
-set(CPACK_PRODUCTBUILD_IDENTIFIER       org.amneziavpn)
-set(CPACK_PREFLIGHT_AMNEZIAVPN_SCRIPT   ${CMAKE_SOURCE_DIR}/deploy/data/macos/post_uninstall.sh)
-set(CPACK_POSTFLIGHT_AMNEZIAVPN_SCRIPT  ${CMAKE_SOURCE_DIR}/deploy/data/macos/post_install.sh)
+set(CPACK_PRODUCTBUILD_IDENTIFIER       app.awarp)
+set(CPACK_PREFLIGHT_AWARP_SCRIPT        ${CMAKE_SOURCE_DIR}/deploy/data/macos/post_uninstall.sh)
+set(CPACK_POSTFLIGHT_AWARP_SCRIPT       ${CMAKE_SOURCE_DIR}/deploy/data/macos/post_install.sh)
 set(CPACK_POSTFLIGHT_UNINSTALL_SCRIPT   ${CMAKE_SOURCE_DIR}/deploy/data/macos/post_uninstall.sh)
 # provide custom CPack.distribution.dist.in
 list(APPEND CMAKE_MODULE_PATH           ${CMAKE_SOURCE_DIR}/deploy/data/macos)
 
 if(LINUX AND NOT ANDROID)
     install(FILES
-        ${CMAKE_SOURCE_DIR}/deploy/data/linux/AmneziaVPN.service
-        ${CMAKE_SOURCE_DIR}/deploy/data/linux/AmneziaVPN.png
-        ${CMAKE_SOURCE_DIR}/deploy/data/linux/AmneziaVPN.desktop
+        ${CMAKE_SOURCE_DIR}/deploy/data/linux/AWARP.service
+        ${CMAKE_SOURCE_DIR}/deploy/data/linux/AWARP.png
+        ${CMAKE_SOURCE_DIR}/deploy/data/linux/AWARP.desktop
         ${CMAKE_SOURCE_DIR}/deploy/data/linux/post_install.sh
         ${CMAKE_SOURCE_DIR}/deploy/data/linux/post_uninstall.sh
         DESTINATION "."
-        COMPONENT AmneziaVPN
+        COMPONENT AWARP
     )
 endif()
 
@@ -70,19 +70,19 @@ if(WIN32)
         ${CMAKE_SOURCE_DIR}/deploy/data/windows/post_install.cmd
         ${CMAKE_SOURCE_DIR}/deploy/data/windows/post_uninstall.cmd
         DESTINATION "."
-        COMPONENT AmneziaVPN
+        COMPONENT AWARP
     )
 endif()
 
 if (APPLE AND NOT IOS AND NOT MACOS_NE)
-    install(FILES ${CMAKE_SOURCE_DIR}/deploy/data/macos/AmneziaVPN.plist
-        DESTINATION "AmneziaVPN.app/Contents/Resources"
-        COMPONENT AmneziaVPN
+    install(FILES ${CMAKE_SOURCE_DIR}/deploy/data/macos/AWARP.plist
+        DESTINATION "AWARP.app/Contents/Resources"
+        COMPONENT AWARP
     )
 endif()
 
 include(CPackIFW)
-cpack_ifw_configure_component(AmneziaVPN
+cpack_ifw_configure_component(AWARP
     VERSION ${AMNEZIAVPN_VERSION}
     RELEASE_DATE ${RELEASE_DATE}
     REQUIRES_ADMIN_RIGHTS
@@ -92,7 +92,7 @@ cpack_ifw_configure_component(AmneziaVPN
 
 include(CPack)
 cpack_add_component(Uninstall
-    DISPLAY_NAME "Uninstall AmneziaVPN"
+    DISPLAY_NAME "Uninstall AWARP"
     REQUIRES_ADMIN_RIGHTS
     DISABLED
 )

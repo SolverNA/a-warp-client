a9c28d66d3a87ecfbbb18a0d34fd89384674b7e1
SolVerNA <gizartv@gmail.com>
2026-06-12 01:39:11 +0300

    chore(warp): обновление скриптов установки и инсталлятора под AWARP

--- deploy/data/windows/post_uninstall.cmd
+++ deploy/data/windows/post_uninstall.cmd
@@ -2,20 +2,20 @@ set AmneziaPath=%~dp0
 echo %AmneziaPath%
 
 rem Define directories for logs
-set "ORG_DIR=%AppData%\AmneziaVPN.ORG"
-set "USER_APP_DIR=%ORG_DIR%\AmneziaVPN"
+set "ORG_DIR=%AppData%\awarp.app"
+set "USER_APP_DIR=%ORG_DIR%\AWARP"
 set "USER_LOG_DIR=%USER_APP_DIR%\log"
-set "SYS_APP_DIR=%ProgramData%\AmneziaVPN"
+set "SYS_APP_DIR=%ProgramData%\AWARP"
 set "SYS_LOG_DIR=%SYS_APP_DIR%\log"
-set "SYS_LOG_FILE=%SYS_LOG_DIR%\AmneziaVPN-service.log"
+set "SYS_LOG_FILE=%SYS_LOG_DIR%\AWARP-service.log"
 
 timeout /t 1
-sc stop AmneziaVPN-service
-sc delete AmneziaVPN-service
-sc stop AmneziaWGTunnel$AmneziaVPN
-sc delete AmneziaWGTunnel$AmneziaVPN
-taskkill /IM "AmneziaVPN-service.exe" /F
-taskkill /IM "AmneziaVPN.exe" /F
+sc stop AWARP-service
+sc delete AWARP-service
+sc stop AWARPTunnel$AWARP
+sc delete AWARPTunnel$AWARP
+taskkill /IM "AWARP-service.exe" /F
+taskkill /IM "AWARP.exe" /F
 
 rem Delete the service log file under ProgramData
 if exist "%SYS_LOG_FILE%" del /F /Q "%SYS_LOG_FILE%"
